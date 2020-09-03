66985d7172c7fa73678e7d5559ebeaef889d4fa7
epyiv <stephen.salyer.ext@bayer.com>
2020-09-02 21:03:29 -0500

    success

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from "react";
 import "./App.css";
 import { Container } from "@material-ui/core";
 import FormUserDetails from "./Components/FormUserDetails";
+import Success from "./Components/Success";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => (
   <Container maxWidth="sm">
-    <FormUserDetails />
+    <Router>
+      <Switch>
+        <Route path="/">
+          <FormUserDetails />
+        </Route>
+        <Route path="/login-successful">
+          <Success />
+        </Route>
+      </Switch>
+    </Router>
   </Container>
 );
 
